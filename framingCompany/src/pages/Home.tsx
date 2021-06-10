d5d88c58cf49bbee8f4a06b656f1e4a264e2da37
Steve Lee <lee.marreros@pucp.pe>
2021-06-10 00:34:18 -0500

    (imges): adds images placeholders

--- framingCompany/src/pages/Home.tsx
+++ framingCompany/src/pages/Home.tsx
@@ -1,1 +1,30 @@
-export {}
+/**
+* 
+* @format
+*/
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+import Colors from '../utils/Colors';
+
+const App = () => {
+  return (
+    <SafeAreaView style={styles.containerApp}>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+ containerApp: {
+   flex: 1,
+   backgroundColor: Colors.indigoBlue,
+ }
+});
+
+export default App;
