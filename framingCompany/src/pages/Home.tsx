0b08e448517185253a7ee1b8229e18ce58a9ceac
Steve Lee <lee.marreros@pucp.pe>
2021-06-10 22:42:41 -0500

    (editing layout): improves layout of editing profile

--- framingCompany/src/pages/Home.tsx
+++ framingCompany/src/pages/Home.tsx
@@ -1,9 +1,9 @@
 /**
-* 
-* @format
-*/
+ *
+ * @format
+ */
 
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -20,58 +20,100 @@ import WelcomeName from '../components/WelcomeName';
 import InputName from '../components/InputName';
 import Colors from '../utils/Colors';
 import ImagePlaceholder from '../utils/ImagePlaceholder';
-const logo = '../images/logo.png'
-const profilePath = '../images/profile.png'
+const logo = '../images/logo.png';
+const profilePath = '../images/profile.png';
+const changeIcon = '../images/change.png';
+const profilePicSize = 220;
 
 const App = () => {
   const [profilePic, setProfilePic] = useState(ImagePlaceholder.profilePic64);
   const [editingMode, setEditingMode] = useState(false);
   const [userName, setUserName] = useState('');
-  
+  const [isRoundedProfile, setShapeProfile] = useState(true);
+
   return (
     <SafeAreaView style={styles.containerApp}>
       <StatusBar barStyle={'light-content'} />
       <View style={styles.header}>
         <View style={styles.logoContainer}>
-          <Image source={require(logo)} style={styles.logo} resizeMode='contain' />
+          <Image
+            source={require(logo)}
+            style={styles.logo}
+            resizeMode="contain"
+          />
         </View>
       </View>
       <View style={styles.profileContainer}>
         <View style={styles.welcomeName}>
-          {editingMode ? 
-            <InputName 
-              userName={userName} 
+          {editingMode ? (
+            <InputName
+              userName={userName}
               editingMode={editingMode}
-              setUserName={setUserName} 
-            /> 
-            : 
-            <WelcomeName title={`Hello${userName === '' ? '' : ' '}${userName}!`}/>
-          }
+              setUserName={setUserName}
+            />
+          ) : (
+            <WelcomeName
+              title={`Hello${userName === '' ? '' : ' '}${userName}!`}
+            />
+          )}
         </View>
         <View style={styles.userPictureContainer}>
-          <View style={styles.userPictureWrap}>
-            <Image source={{uri: `data:image/jpeg;base64,${profilePic}`}} style={styles.userPicture} resizeMode='cover' />
+          <View style={styles.top}>
+            <View style={styles.squareRoundButtons}>
+              {editingMode && (
+                <TouchableOpacity
+                  onPress={() => setShapeProfile(false)}
+                  style={[styles.buttonLeft, styles.squareButton]}
+                />
+              )}
+              {editingMode && (
+                <TouchableOpacity
+                  onPress={() => setShapeProfile(true)}
+                  style={[styles.buttonLeft, styles.roundedButton]}
+                />
+              )}
+            </View>
+            <View
+              style={[
+                styles.userPictureWrap,
+                isRoundedProfile ? styles.userPictureWrapRounded : null,
+              ]}>
+              <Image
+                source={{uri: `data:image/jpeg;base64,${profilePic}`}}
+                style={[
+                  styles.userPicture,
+                  isRoundedProfile ? styles.userPictureRounded : null,
+                ]}
+                resizeMode="cover"
+              />
+            </View>
+            <View style={styles.changePictureButton}>
+              {editingMode && (
+                <TouchableOpacity
+                  onPress={() => {
+                    ImagePicker.openPicker({
+                      width: 300,
+                      height: 400,
+                      cropping: true,
+                      includeBase64: true,
+                    }).then(image => {
+                      setProfilePic(image.data);
+                    });
+                  }}
+                  activeOpacity={0.75}>
+                  <Image
+                    style={styles.changeIcon}
+                    source={require(changeIcon)}
+                  />
+                </TouchableOpacity>
+              )}
+            </View>
           </View>
-          {editingMode && 
-            <TouchableOpacity onPress={() => {
-                ImagePicker.openPicker({
-                  width: 300,
-                  height: 400,
-                  cropping: true,
-                  includeBase64: true,
-                }).then(image => {
-                  setProfilePic(image.data)
-                });
-              }
-            }>
-              <Text style={styles.newPictureText}>Chose a different picture</Text>
-            </TouchableOpacity> 
-          }
         </View>
       </View>
       <View style={styles.button}>
         <Button
-          title={editingMode ? "Publish" : "Edit Profile"} 
+          title={editingMode ? 'Publish' : 'Edit Profile'}
           onPress={() => setEditingMode(!editingMode)}
         />
       </View>
@@ -80,66 +122,102 @@ const App = () => {
 };
 
 const styles = StyleSheet.create({
- containerApp: {
-   flex: 1,
-   backgroundColor: Colors.indigoBlue,
-   flexDirection: 'column',
- },
- header: {
-  flex: 2,
-  justifyContent: 'center',
-  alignItems: 'center',
- },
- logoContainer: {
-   height: 145,
-   width: 145,
-   justifyContent: 'center',
-   alignItems: 'center',
-   borderWidth: 5,
-   borderColor: Colors.seafoamBlue,
- },
- logo: {
-   flex: 1,
- },
- profileContainer: {
-  flex: 5,
-  flexDirection: 'column',
- },
- welcomeName: {
-  flex: 1,
-  justifyContent: 'center',
-  alignItems: 'center',
- },
- userPictureContainer: {
-  flex: 4,
-  alignItems: 'center',
- },
- userPictureWrap: {
-  width: 250,
-  height: 250,
-  borderRadius: 250 / 2,
-  justifyContent: 'center',
-  alignItems: 'center',
-  borderWidth: 5,
-  borderColor: Colors.seafoamBlue,
- },
- userPicture: {
-  width: 240,
-  height: 240,
-  borderRadius: 250 / 2,
- },
- newPictureText: {
-   color: Colors.seafoamBlue,
-   fontSize: 14,
-   fontWeight: 'bold',
-   fontFamily: 'Helvetica',
-   marginTop: 20,
- },
- button: {
-  flex: 1,
-  justifyContent: 'center',
-  alignItems: 'center'
- },
+  containerApp: {
+    flex: 1,
+    backgroundColor: Colors.indigoBlue,
+    flexDirection: 'column',
+  },
+  header: {
+    flex: 2,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  logoContainer: {
+    height: 145,
+    width: 145,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 5,
+    borderColor: Colors.seafoamBlue,
+  },
+  logo: {
+    flex: 1,
+  },
+  profileContainer: {
+    flex: 5,
+    flexDirection: 'column',
+  },
+  welcomeName: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  userPictureContainer: {
+    flex: 4,
+    alignItems: 'center',
+  },
+  top: {
+    flexDirection: 'row',
+    width: '100%',
+  },
+  squareRoundButtons: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+  },
+  buttonLeft: {
+    width: 35,
+    height: 35,
+    backgroundColor: Colors.melon,
+    borderWidth: 5,
+    borderColor: Colors.seafoamBlue,
+    opacity: 0.75,
+  },
+  squareButton: {},
+  roundedButton: {
+    borderRadius: 35 / 2,
+  },
+  userPictureWrap: {
+    width: profilePicSize,
+    height: profilePicSize,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: Colors.seafoamBlue,
+    borderWidth: 5,
+  },
+  userPictureWrapRounded: {
+    borderRadius: profilePicSize / 2,
+  },
+  changePictureButton: {
+    flex: 1,
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  changeIcon: {
+    width: 35,
+    height: 35,
+  },
+  userPicture: {
+    width: profilePicSize - 10,
+    height: profilePicSize - 10,
+  },
+  userPictureRounded: {
+    borderRadius: profilePicSize - 10 / 2,
+  },
+  newPictureText: {
+    color: Colors.seafoamBlue,
+    fontSize: 14,
+    fontWeight: 'bold',
+    fontFamily: 'Helvetica',
+    marginTop: 20,
+  },
+  button: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 });
 
 export default App;
--- framingCompany/src/utils/Colors.tsx
+++ framingCompany/src/utils/Colors.tsx
@@ -1,4 +1,7 @@
 export default {
   indigoBlue: '#3C189F',
   seafoamBlue: '#7DD1C7',
+  melon: '#ED806D',
+  lightRose: '#F6C8CF',
+  sunYellow: '#F6DD30'
 };
